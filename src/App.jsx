bc98b121dbb5396c554e4c69cac2c4821d3e2cb3
FahimCC <ffnasif@gmail.com>
2023-11-03 09:43:18 +0600

    removed react-dnd component

--- src/App.jsx
+++ src/App.jsx
@@ -101,56 +101,6 @@ const App = () => {
     setSelectedPhotos([]);
   };
 
-  //TODO:
-  const handleDnd = async (results) => {
-    const { source, destination, type } = results;
-    console.log(source, destination, type);
-    if (!destination) return;
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    )
-      return;
-    if (type === "group") {
-      const reorderedStores = [...photos];
-      const storeSourceIndex = source.index;
-      const storeDestinationIndex = destination.index;
-      const [removedStore] = reorderedStores.splice(storeSourceIndex, 1);
-      reorderedStores.splice(storeDestinationIndex, 0, removedStore);
-      const reorderedPhotos = await Promise.all(
-        reorderedStores.map((photo, index) => {
-          return { ...photo, Order: index };
-        })
-      );
-      console.log(reorderedPhotos);
-      setPhotos(reorderedPhotos);
-      console.log("reorderedPhotos::: ", await reorderedPhotos);
-    }
-  };
-
-  // const handleDnd = (results) => {
-  //   const { source, destination, type } = results;
-  //   console.log(source, destination, type);
-  //   if (!destination) return;
-  //   if (
-  //     source.droppableId === destination.droppableId &&
-  //     source.index === destination.index
-  //   )
-  //     return;
-  //   if (type === "group") {
-  //     const reorderedStores = [...photos];
-  //     const [movedItem] = reorderedStores.splice(source.index, 1);
-  //     reorderedStores.splice(destination.index, 0, movedItem);
-
-  //     const reorderedPhotos = reorderedStores.map((photo, index) => ({
-  //       ...photo,
-  //       order: index + 1,
-  //     }));
-
-  //     setPhotos(reorderedPhotos);
-  //   }
-  // };
-
   return (
     <>
       <div className="bg-slate-100 h-full font-openSans">
@@ -189,92 +139,67 @@ const App = () => {
               </div>
             </div>
             <hr />
-            {/* //TODO:  */}
-            <DragDropContext onDragEnd={handleDnd}>
-              <Droppable droppableId="ROOT" type="group">
-                {(provided) => (
-                  <div
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    className="p-4 md:p-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3 md:gap-6"
-                  >
-                    {photos?.map((photo, index) => (
-                      <Draggable
-                        key={index}
-                        draggableId={photo?.id}
-                        index={photo?.order - 1}
-                      >
-                        {(provided) => (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            className={`${
-                              photo.order === 1 && "col-span-2 row-span-2"
-                            }`}
-                          >
-                            <label className="relative">
-                              {console.log("photo?.id", photo?.id)}
-                              <div
-                                className={`smooth rounded-lg ${
-                                  selectedPhotos?.includes(photo?.id)
-                                    ? "opacity-50"
-                                    : "hover:bg-zinc-500"
-                                }`}
-                              >
-                                <img
-                                  className="w-full h-full rounded-lg border-2 border-[#c9cbcf] mix-blend-multiply	"
-                                  src={photo.image}
-                                  alt={`image-${index}`}
-                                />
-                              </div>
-                              <div className="">
-                                <input
-                                  type="checkbox"
-                                  name="gender"
-                                  id={photo.id}
-                                  value={photo.id}
-                                  className={` ${
-                                    selectedPhotos?.includes(photo.id)
-                                      ? "block"
-                                      : "hover:block hidden"
-                                  }   lg:mr-8 w-[14px] h-[14px] lg:w-[18px] lg:h-[18px] cursor-pointer accent-primary absolute top-[14px] left-[14px]  md:top-[18px] md:left-[18px]`}
-                                  onChange={() =>
-                                    handleSelectedPhotos(photo.id)
-                                  }
-                                />
-                              </div>
-                            </label>
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
-                    {provided.placeholder}
+
+            <div className="p-4 md:p-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3 md:gap-6">
+              {photos?.map((photo, index) => (
+                <div
+                  key={index}
+                  className={`${photo.order === 1 && "col-span-2 row-span-2"}`}
+                >
+                  <label className="relative">
+                    {console.log("photo?.id", photo?.id)}
                     <div
-                      className={` cursor-pointer smooth rounded-lg border-2 border-[#c9cbcf] border-dashed bg-[#f9f9f9] h-full py-8 md:py-10`}
+                      className={`smooth rounded-lg ${
+                        selectedPhotos?.includes(photo?.id)
+                          ? "opacity-50"
+                          : "hover:bg-zinc-500"
+                      }`}
                     >
-                      <div className="squeeze smooth flex flex-col items-center justify-center gap-1 lg:gap-4 h-full">
-                        <svg
-                          stroke="currentColor"
-                          fill="currentColor"
-                          strokeWidth="0"
-                          viewBox="0 0 16 16"
-                          height="1.5em"
-                          width="1.5em"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"></path>
-                          <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"></path>
-                        </svg>
-                        <p className="font-semibold text-center text-sm  md:text-base">
-                          Add Images
-                        </p>
-                      </div>
+                      <img
+                        className="w-full h-full rounded-lg border-2 border-[#c9cbcf] mix-blend-multiply	"
+                        src={photo.image}
+                        alt={`image-${index}`}
+                      />
                     </div>
-                  </div>
-                )}
-              </Droppable>
-            </DragDropContext>
+                    <div className="">
+                      <input
+                        type="checkbox"
+                        name="gender"
+                        id={photo.id}
+                        value={photo.id}
+                        className={` ${
+                          selectedPhotos?.includes(photo.id)
+                            ? "block"
+                            : "hover:block hidden"
+                        }   lg:mr-8 w-[14px] h-[14px] lg:w-[18px] lg:h-[18px] cursor-pointer accent-primary absolute top-[14px] left-[14px]  md:top-[18px] md:left-[18px]`}
+                        onChange={() => handleSelectedPhotos(photo.id)}
+                      />
+                    </div>
+                  </label>
+                </div>
+              ))}
+              <div
+                className={` cursor-pointer smooth rounded-lg border-2 border-[#c9cbcf] border-dashed bg-[#f9f9f9] h-full py-8 md:py-10`}
+              >
+                <div className="squeeze smooth flex flex-col items-center justify-center gap-1 lg:gap-4 h-full">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 16 16"
+                    height="1.5em"
+                    width="1.5em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"></path>
+                    <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"></path>
+                  </svg>
+                  <p className="font-semibold text-center text-sm  md:text-base">
+                    Add Images
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
